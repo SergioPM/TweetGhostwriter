af1695c10aa019e37af096aed8e8c35cd98bd678
Unknown <spablomendoza@mail.csuchico.edu>
2017-04-06 23:47:44 -0700

    Added more Java Script

--- ghostwriter.js
+++ ghostwriter.js
@@ -1,6 +1,17 @@
 function writeTweet()
 {
-  
+  var randIndex;
+  var sentence;
+  var intro = ["Have you ever wondered", "Why is it that", "What comes to mind when", "At the end of the tunnel"];
+  var body = ["the sky", "you live", "our conscious", "the future", "life"];
+  var end = ["is mysterious.","to die."];
+  randIndex = randomUpTo(3,0);
+  sentence = intro[randIndex];
+  randIndex = randomUpTo(4,0);
+  sentence += body[randIndex];
+  randIndex = randomUpTo(1,0);
+  sentence += end[randIndex];
+  document.getElementById("tweet").innerHTML = sentence;
 }
 
 //Generates a random whole number between 0 and max (inclusive)
